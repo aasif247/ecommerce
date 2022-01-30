2d9b15bbb5e7e614f498122150bafdd6c37c45ec
aasif247 <47910560+aasif247@users.noreply.github.com>
2022-01-31 00:53:42 +0600

    admin logout option

--- resources/views/admin/admin_master.blade.php
+++ resources/views/admin/admin_master.blade.php
@@ -8,7 +8,7 @@
     <meta name="author" content="">
     <link rel="icon" href="{{ asset('backend/images/favicon.ico') }}">
 
-    <title>Gadget Shed BD Admin</title>
+    <title>GSBD Admin</title>
     
 	<!-- Vendors Style-->
 	<link rel="stylesheet" href="{{ asset('backend/css/vendors_css.css') }}">
--- routes/web.php
+++ routes/web.php
@@ -28,6 +28,8 @@ Route::middleware(['auth:sanctum,admin', 'verified'])->get('/admin/dashboard', f
     return view('admin.index');
 })->name('dashboard');
 
+// Admin routes
+Route::get('/admin/logout', [AdminController::class, 'destroy'])->name('admin.logout');
 
 Route::middleware(['auth:sanctum,web', 'verified'])->get('/dashboard', function () {
     return view('dashboard');
