b62192ad80580da0fe146ca3aeabb1fe0d299642
aasif247 <47910560+aasif247@users.noreply.github.com>
2022-02-18 17:08:21 +0600

    Brand Page Design Part-2 - Using Data Table For Showing All Brands

--- resources/views/Backend/Brand/brand_view.blade.php
+++ resources/views/Backend/Brand/brand_view.blade.php
@@ -2,4 +2,87 @@
 
 @section('admin')
 
+    <div class="container-full">
+      <!-- Content Header (Page header) -->
+      <div class="content-header">
+          <div class="d-flex align-items-center">
+              <div class="mr-auto">
+                  <h3 class="page-title">Brands</h3>
+                  <div class="d-inline-block align-items-center">
+                      <nav>
+                          <ol class="breadcrumb">
+                              <li class="breadcrumb-item"><a href="#"><i class="mdi mdi-home-outline"></i></a></li>
+                              <li class="breadcrumb-item" aria-current="page">Brands</li>
+                              <li class="breadcrumb-item active" aria-current="page">All Brands</li>
+                          </ol>
+                      </nav>
+                  </div>
+              </div>
+          </div>
+      </div>
+
+      <!-- Main content -->
+      <section class="content">
+        <div class="row">
+
+          <div class="col-12">
+
+           <div class="box">
+              <div class="box-header with-border">
+                <h3 class="box-title">Brand List</h3>
+              </div>
+              <!-- /.box-header -->
+              <div class="box-body">
+                  <div class="table-responsive">
+                    <table id="example1" class="table table-bordered table-striped">
+                      <thead>
+                          <tr>
+                              <th>Brand En</th>
+                              <th>Brand Bn</th>
+                              <th>Image</th>
+                              <th>Action</th>
+                          </tr>
+                      </thead>
+                      <tbody>
+                        @foreach($brands as $brand)
+                          <tr>
+                              <td>{{ $brand->brand_name_en}}</td>
+                              <td>{{ $brand->brand_name_bn}}</td>
+                              <td><img src="{{ asset($brand->brand_image) }}" style= "width: 70px; height: 40px;">
+                              </td>
+                              {{-- <td>{{ $brand->action}}</td> --}}
+                              <td>
+                                <div class="row">
+                                    <div>
+                                        <a href="#" type="button" class="btn btn-primary m-2">Edit</a>
+                                    </div>
+
+                                    <div>
+                                        <form action="#" method="POST">
+                                        @csrf
+                                        @method('DELETE')
+                                        <input type="submit" name="submit" value="Delete" class="btn btn-danger m-2">
+                                        </form>
+                                    </div>
+                                </div>
+                            </td>
+                          </tr>
+                         @endforeach 
+                      </tbody>
+                    </table>
+                  </div>
+              </div>
+              <!-- /.box-body -->
+            </div>
+                     
+          </div>
+          <!-- /.col -->
+        </div>
+        <!-- /.row -->
+      </section>
+      <!-- /.content -->
+    
+    </div>
+
+
 @endsection
