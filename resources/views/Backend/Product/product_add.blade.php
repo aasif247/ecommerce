9f5f923a2fee3d50f6863ed70dd45d1c0dd17dfc
aasif247 <47910560+aasif247@users.noreply.github.com>
2022-02-27 12:19:55 +0600

    mistakenly comment out the change

--- resources/views/Backend/Product/product_add.blade.php
+++ resources/views/Backend/Product/product_add.blade.php
@@ -415,7 +415,7 @@
             type:"GET",
             dataType:"json",
             success:function(data){
-              // $('select[name="subsubcategory_id"]').html('');
+              $('select[name="subsubcategory_id"]').html('');
               var e =$('select[name="subcategory_id"]').empty();
               $.each(data,function(key,value){
                 $('select[name="subcategory_id"]').append('<option value="'+ value.id +'">' + value.subcategory_name_en + '</option>');
